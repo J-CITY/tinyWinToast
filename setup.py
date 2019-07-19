9ab828c6969e3a432b056b9945c4f61bea3f55dd
Daniil <daniil.glushchenko1995@gmail.com>
2019-07-19 22:57:11 +1000

    update setup

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ setup(
 	name='tinyWinToast',
 	version=tinyWinToast.__version__,
 	packages=find_packages(),
+	url="https://github.com/J-CITY/tinyWinToast",
 	long_description=open(join(dirname(__file__), 'README.txt')).read(),
+	description=(
+		"Tiny Python library for displaying "
+		"Windows toasts"
+	),
+	author="J-CITY (Glushchenko Daniil)"
 )
